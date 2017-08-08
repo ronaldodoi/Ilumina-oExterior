62dc995207deff0981f5105277aa197b42a9977c
Igor Altieres <igoraltieres@gmail.com>
2017-08-08 11:30:06 -0300

    Esqueleto andando em volta do fogo

---
+++ Assets/Scripts/SkeletonMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMove : MonoBehaviour {
+
+    public float velocidade = 1;
+    public List<Vector3> lista = new List<Vector3>();
+    
+    int destino;
+
+    void Start () {
+        destino = 0;
+        StartCoroutine("MoverAteProximoPonto");
+    }
+
+    IEnumerator MoverAteProximoPonto() {
+        transform.LookAt(lista[destino]);
+
+        while(transform.position != lista[destino]){
+            transform.position = Vector3.MoveTowards(transform.position, lista[destino], velocidade * Time.deltaTime);
+			yield return null;
+        }
+    
+		destino++;
+		if (destino >= lista.Count){
+			destino = 0;
+		}
+
+		StartCoroutine("MoverAteProximoPonto");
+	}
+}	
